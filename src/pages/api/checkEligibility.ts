8e4c904d4b489d8b360c248207f10dd218d12686
gayatrigt <taleygayatri@gmail.com>
2024-06-23 18:41:40 +0530

    points

--- src/pages/api/checkEligibility.ts
+++ src/pages/api/checkEligibility.ts
@@ -37,7 +37,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             isSplitter: boolean,
             isFollowingChannel: boolean
         ): number => {
-            let maxAllowancePoints = 100;
+            const maxAllowancePoints = 100;
 
             // if (isPowerBadgeHolder) {
             //     maxAllowancePoints = 100;
